fa83f114599e5f2529cb5f5d384adda7d20fab48
Thomas Bulacio <thomasbulacio.motog@gmail.com>
2023-04-02 21:54:35 -0300

    SegundoCommit

--- src/components/CardProductos/CardProductos.js
+++ src/components/CardProductos/CardProductos.js
@@ -1,26 +1,26 @@
-import * as React from "react";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
+// import * as React from "react";
+// import Card from "@mui/material/Card";
+// import CardContent from "@mui/material/CardContent";
+// import CardMedia from "@mui/material/CardMedia";
+// import Typography from "@mui/material/Typography";
+// import { CardActionArea } from "@mui/material";
 
-const CardProductos = ({ img, name, precio, backgroundColor }) => {
-  return (
-    <Card sx={{ maxWidth: 345, backgroundColor }}>
-      <CardActionArea>
-        <CardMedia component="img" image={img} alt="TV" />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {name}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {precio}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-  );
-};
+// const CardProductos = ({ img, name, precio, backgroundColor }) => {
+//   return (
+//     <Card sx={{ maxWidth: 345, backgroundColor }}>
+//       <CardActionArea>
+//         <CardMedia component="img" image={img} alt="TV" />
+//         <CardContent>
+//           <Typography gutterBottom variant="h5" component="div">
+//             {name}
+//           </Typography>
+//           <Typography variant="body2" color="text.secondary">
+//             {precio}
+//           </Typography>
+//         </CardContent>
+//       </CardActionArea>
+//     </Card>
+//   );
+// };
 
-export default CardProductos;
+// export default CardProductos;
